8d5339fe5a644c96c026bbb710df5eca4daff783
JaquelineGDS <jaqueline_gd@outlook.com>
2019-07-14 13:44:59 -0300

    eslint

--- src/cipher.js
+++ src/cipher.js
@@ -1,53 +1,51 @@
-document.querySelector('.btn-encode').addEventListener('click', () => controllerFunctions(encode));
-document.querySelector('.btn-decode').addEventListener('click', () => controllerFunctions(decode));
-
 const controllerFunctions = (functionExecute) => {
-    const text = document.querySelector('.text').value;
-    const offset = parseInt(document.querySelector('.offset').value);
-    clear();
-    const result = functionExecute(text, offset);
-    document.querySelector('.text').value = result;
-}
+  const text = document.querySelector('.text').value;
+  const offset = parseInt(document.querySelector('.offset').value);
+  clear();
+  const result = functionExecute(text, offset);
+  document.querySelector('.text').value = result;
+};
 
-const mod = (n, m) => {
-    return ((n % m) + m) % m;
-}
+const mod = (number, alphabetSize) => ((number % alphabetSize) + alphabetSize) % alphabetSize;
 
-const encode = (string, offset) => {
-    let result = [];
-    let number = 0;
-    for (let i in string) {
-       let ascCode = string.charCodeAt(i);
-        if (ascCode >= 65 && ascCode <= 90) {
-            number = mod((ascCode - 65 + offset), 26) + 65;
-        } else if (ascCode >= 97 && ascCode <= 122) {
-            number = mod((ascCode - 97 + offset), 26) + 97;
-        } else{
-            number = ascCode;
-        }
-        result.push(String.fromCharCode(number));
+const encode = (text, offset) => {
+  let result = [];
+  let number = 0;
+  for (let i in text) {
+    let ascCode = text.charCodeAt(i);
+    if (ascCode >= 65 && ascCode <= 90) {
+      number = mod((ascCode - 65 + offset), 26) + 65;
+    } else if (ascCode >= 97 && ascCode <= 122) {
+      number = mod((ascCode - 97 + offset), 26) + 97;
+    } else {
+      number = ascCode;
     }
-    return result.join("");
-}
+    result.push(String.fromCharCode(number));
+  }
+  return result.join('');
+};
 
-const decode = (string, offset) => {
-    let result = [];
-    let number = 0;
-    for (let i in string) {
-       let ascCode = string.charCodeAt(i);
-        if (ascCode >= 65 && ascCode <= 90) {
-            number = mod((ascCode - 65 - offset), 26) + 65;
-        } else if (ascCode >= 97 && ascCode <= 122) {
-            number = mod((ascCode - 97 - offset), 26) + 97;
-        } else{
-            number = ascCode;
-        }
-        result.push(String.fromCharCode(number));
+const decode = (text, offset) => {
+  let result = [];
+  let number = 0;
+  for (let i in text) {
+    let ascCode = text.charCodeAt(i);
+    if (ascCode >= 65 && ascCode <= 90) {
+      number = mod((ascCode - 65 - offset), 26) + 65;
+    } else if (ascCode >= 97 && ascCode <= 122) {
+      number = mod((ascCode - 97 - offset), 26) + 97;
+    } else {
+      number = ascCode;
     }
-    return result.join("");
-}
+    result.push(String.fromCharCode(number));
+  }
+  return result.join('');
+};
 
 const clear = () => {
-    document.querySelector('.text').value = "";
-    document.querySelector('.offset').value = "";
-}
+  document.querySelector('.text').value = '';
+  document.querySelector('.offset').value = '';
+};
+
+document.querySelector('.btn-encode').addEventListener('click', () => controllerFunctions(encode));
+document.querySelector('.btn-decode').addEventListener('click', () => controllerFunctions(decode));
--- src/tests.js
+++ src/tests.js
@@ -1,21 +1,21 @@
 
-let test1 = encode('abc', 1);
+const test1 = encode('abc', 1);
 if (test1 === 'bcd') {
-console.log('O parametro abc com deslocamento de 1 da função encode foi executado corretamente: ' + test1 );
+  console.log('O parametro abc com deslocamento de 1 da função encode foi executado corretamente: ' + test1 );
 } else {
-console.log('O parametro abc com deslocamento de 1 da função encode não foi executado corretamente.');
+  console.log('O parametro abc com deslocamento de 1 da função encode não foi executado corretamente.');
 }
 
-let test2 = encode('Jaqueline', 1);
+const test2 = encode('Jaqueline', 1);
 if (test2 === 'Kbrvfmjof') {
-console.log('O parametro Jaqueline com deslocamento de 1 da função encode foi executado corretamente: ' + test2);
+  console.log('O parametro Jaqueline com deslocamento de 1 da função encode foi executado corretamente: ' + test2);
 } else {
-console.log('O parametro Jaqueline com deslocamento de 1 da função encode não foi executado corretamente. ');
+  console.log('O parametro Jaqueline com deslocamento de 1 da função encode não foi executado corretamente. ');
 }
 
-let test3 = decode('bcd', 1);
+const test3 = decode('bcd', 1);
 if (test3 === 'abc') {
-console.log('O parametro bcd com deslocamento de 1 da função decode foi executado corretamente: ' + test3);
+  console.log('O parametro bcd com deslocamento de 1 da função decode foi executado corretamente: ' + test3);
 } else {
-console.log('O parametro bcd com deslocamento de 1 não foi executado corretamente . ')
+  console.log('O parametro bcd com deslocamento de 1 não foi executado corretamente . ')
 }
